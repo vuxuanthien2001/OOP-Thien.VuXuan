f22efb5f84690310c7e4c23a42de40bd3a06a1ec
Thien.VuXuan <vuxuanthien2001@gmail.com>
2022-07-01 21:00:04 +0700

    buoi2oop

--- src/Entity/IEnity.java
+++ src/Entity/IEnity.java
@@ -1,13 +1,28 @@
 package Entity;
 
 public interface IEnity {
-
+    /**
+     * get Id
+     * @return
+     */
     public int getId();
 
+    /**
+     * set id
+     * @param id
+     */
     public void setId(int id);
 
+    /**
+     * get name
+     * @return
+     */
     public String getName();
 
+    /**
+     * set name
+     * @param name
+     */
     public void setName(String name);
 
 }
